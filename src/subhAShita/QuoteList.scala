55255bf749cffa8747bd86bba04723f2a997647c
vvasuki <vishvas.vasuki@gmail.com>
2017-03-27 15:15:53 -0700

    asdf

--- src/subhAShita/QuoteList.scala
+++ src/subhAShita/QuoteList.scala
@@ -3,6 +3,7 @@ package subhAShita
 import java.io.FileReader
 import java.util
 
+import net.liftweb.json.Serialization
 import org.apache.commons.csv.{CSVFormat, CSVRecord, QuoteMode}
 import org.slf4j.LoggerFactory
 import sanskritnlp.quote.{TopicAnnotation, _}
@@ -23,27 +24,32 @@ object vishvasaPriyaSamskritaPadyani
   override val source = sourceHelper.getSanskritDevanaagariiSource("विश्वास-प्रिय-संस्कृत-पद्यानि", "विश्वासः"::Nil)
   def hasNext(): Boolean = records.hasNext
 
-  def next(): QuoteInfo = {
+  def next(): QuoteWithInfo = {
     val record = records.next()
     log debug record.toMap.toString
     if (record.get("भाषा") == "" || record.get("भाषा") == "संस्कृतम्") {
-//      val originAnnotations = OriginAnnotation(source=source,
-//        origin = Source(ScriptRendering(text = record.get("स्रोतांसि"), scheme = transliterator.scriptDevanAgarI),
-//          List(ScriptRendering(text = record.get("वक्ता"), scheme = transliterator.scriptDevanAgarI))))::Nil
       val quoteText = QuoteText(
         ScriptRendering(text = record.get("सुभाषितम्"), scheme = transliterator.scriptDevanAgarI)::Nil,
         language = Language("sa"))
       var descriptionAnnotations = List[DescriptionAnnotation]()
+      // Don't add empty strings as descriptions.
       if (record.get("विवरणम्").nonEmpty) {
-        descriptionAnnotations = DescriptionAnnotation(textKey=quoteText.key, source, ScriptRendering(text = record.get("विवरणम्")))::Nil
+        descriptionAnnotations = DescriptionAnnotation(
+          textKey=quoteText.key, source,
+          QuoteWithInfo(new QuoteText(text = record.get("विवरणम्"))))::Nil
       }
-      val subhashita = QuoteInfo(quoteText,
+
+      val author = record.get("वक्ता")
+      var originAnnotation = OriginAnnotation(textKey=quoteText.key, source=source,
+        origin = sourceHelper.fromAuthor(author = author))
+      val subhashita = QuoteWithInfo(quoteText,
         descriptionAnnotations=descriptionAnnotations,
         ratingAnnotations = RatingAnnotation(textKey=quoteText.key, source=source, overall = Rating(5))::Nil,
         topicAnnotations = record.get("विषयः").split(",").map(_.trim).map(
           x => TopicAnnotation(textKey=quoteText.key, source=source,
             topic = Topic(ScriptRendering(text = x, scheme = transliterator.scriptDevanAgarI),
-              language = Language("sa")))).toList
+              language = Language("sa")))).toList,
+        originAnnotations = originAnnotation::Nil
       )
       return subhashita
     } else {
